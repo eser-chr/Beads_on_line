e1ea426284d0f19733db1e8bdbeccee7e8a0f7a7
Christoforos <chriseseroglou@gmail.com>
2023-08-26 20:16:35 +0200

    123

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <algorithm> 
 using namespace std;
 
+//This is a git comment
+
 // MAIN CONSTANTS OF THE SIMULATION
 const int SIMS=10; // Number of simulations for a setup so that we have better statistics.
 
